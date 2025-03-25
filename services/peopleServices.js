bf5d4f30a7d68dc536eb0fb3d09e5bae6949cedb
Ronald Landheer-Cieslak <RonaldLandheerCieslak@eaton.com>
2025-03-25 17:42:03 +0000

    rewrite the people service to use the new table client, injected by the new middleware

--- services/peopleServices.js
+++ services/peopleServices.js
@@ -1,37 +1,64 @@
-module.exports = () => {
-    let person = [
-        { id: 0, name: 'Luiz', age: 32 },
-        { id: 1, name: 'Peter', age: 26 }
-    ];
+const { v4: uuidv4 } = require('uuid');
 
-    const searchPeople = (req, res, next) => {
+module.exports = () => {
+    const searchPeople = async (req, res, next) => {
         if (!req.query.name) return next();
 
-        const results = person.filter(person => person.name.includes(req.query.name));
+        const entities = await req.tableClient.listEntities({
+            queryOptions: { filter: `name eq '${req.query.name}'` }
+        });
+        const results = [];
+        for await (const entity of entities) {
+            const { age, id, name, ...rest } = entity;
+            results.push({age, id, name});
+        }
+
         return res.status(200).json(results);
     };
 
-    const getAllPeople = (req, res, next) => {
-        console.log("getAllPeople: " + JSON.stringify(person));
-        return res.status(200).json(person);
-    };
-
-    const getById = (req, res, next) => {
-        const { id } = req.params;
-        const result = person.find(person => person.id === parseInt(id, 10));
-
-        if (!result) {
-            return next({ status: 404, message: 'Person not found' });
+    const getAllPeople = async (req, res, next) => {
+        const entities = await req.tableClient.listEntities();
+        const results = [];
+        for await (const entity of entities) {
+            const { age, id, name, ...rest } = entity;
+            results.push({age, id, name});
         }
 
-        return res.status(200).json(result);
+        return res.status(200).json(results);
     };
 
-    const createPeople = (req, res, next) => {
-        const { name, age } = req.body;
-        const id = person.length;
+    const getById = async (req, res, next) => {
+        const { id } = req.params;
 
-        person.push({ id, name, age });
+        const entities = await req.tableClient.listEntities({
+            queryOptions: { filter: `id eq '${id}'` }
+        });
+        const results = [];
+        for await (const entity of entities) {
+            const { age, id, name, ...rest } = entity;
+            results.push({age, id, name});
+        }
+
+        return res.status(200).json(results);
+    };
+
+    const createPeople = async (req, res, next) => {
+        const { name, age } = req.body;
+
+        // this is pretty bad, but it's just an example and the "legacy" code we're replacing uses a number incremental
+        //  ID. The alternative would be to store the ID in anothe table, but I don't have a SAS for that. Another 
+        // alternative would be to use the partition key (i.e. a UUID) but that would break compatibility.
+        const entities = await req.tableClient.listEntities();
+        let maxId = 0;
+        for await (const entity of entities) {
+            if (entity.id && !isNaN(entity.id)) {
+                maxId = Math.max(maxId, parseInt(entity.id, 10));
+            }
+        }
+        const id = maxId + 1;
+
+        person = { id, name, age, partitionKey: uuidv4(), rowKey: uuidv4() };
+        await req.tableClient.createEntity(person);
 
         res.status(201).json({ id, name, age });
     };
